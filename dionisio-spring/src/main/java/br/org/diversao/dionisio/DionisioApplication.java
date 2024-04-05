fbf7456e9c927e0caf9d6fbdf0b9bff3c21a4da8
Matheus Rebola <matheus.rebola@outlook.com>
2024-04-05 16:20:38 -0300

    first commit

---
+++ dionisio-spring/src/main/java/br/org/diversao/dionisio/DionisioApplication.java
@@ -0,0 +1,13 @@
+package br.org.diversao.dionisio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DionisioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DionisioApplication.class, args);
+	}
+
+}
---
+++ dionisio-spring/src/test/java/br/org/diversao/dionisio/DionisioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.diversao.dionisio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DionisioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
